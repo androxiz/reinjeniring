d67d5cba4367c3a79139c1e05cb7629d2b2ace14
Danyil Muzyria <8466486@stud.nau.edu.ua>
2025-10-08 16:28:24 +0300

    Tests for NetSdrMessageHelper methods have been added
    New tests have been added to `NetSdrMessageHelperTests` to verify
    the correctness of the `GetSamples`, `GetControlItemMessage`,
    and `TranslateHeader` methods. The obsolete `//TODO` comment has been removed.
    The tests cover cases with valid and invalid data,
    as well as checking exceptions and boundary conditions.

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -64,6 +64,66 @@ namespace NetSdrClientAppTests
             Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
         }
 
-        //TODO: add more NetSdrMessageHelper tests
+        [Test]
+        public void GetSamples_ValidSampleSize_ReturnsCorrectValues()
+        {
+            // Arrange
+            ushort sampleSize = 16; // 2 bytes
+            byte[] body = new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+
+            // Act
+            var samples = NetSdrMessageHelper.GetSamples(sampleSize, body).ToList();
+
+            // Assert
+            Assert.That(samples.Count, Is.EqualTo(3));
+            Assert.That(samples[0], Is.EqualTo(BitConverter.ToInt32(new byte[] { 0x01, 0x02, 0x00, 0x00 })));
+            Assert.That(samples[1], Is.EqualTo(BitConverter.ToInt32(new byte[] { 0x03, 0x04, 0x00, 0x00 })));
+            Assert.That(samples[2], Is.EqualTo(BitConverter.ToInt32(new byte[] { 0x05, 0x06, 0x00, 0x00 })));
+        }
+
+        [Test]
+        public void GetSamples_InvalidSampleSize_ThrowsException()
+        {
+            // Arrange
+            ushort sampleSize = 40; // 5 bytes - invalid
+            byte[] body = new byte[] { 1, 2, 3, 4, 5 };
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => NetSdrMessageHelper.GetSamples(sampleSize, body).ToList());
+        }
+
+        [Test]
+        public void GetControlItemMessage_ExceedMaxLength_ThrowsException()
+        {
+            // Arrange
+            var type = NetSdrMessageHelper.MsgTypes.Ack;
+            var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
+            int parametersLength = 8192; // Exceeds max message length
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() =>
+                NetSdrMessageHelper.GetControlItemMessage(type, code, new byte[parametersLength]));
+        }
+
+        [Test]
+        public void TranslateHeader_DataItem0ZeroLength_SetsMaxDataItemLength()
+        {
+            var method = typeof(NetSdrClientApp.Messages.NetSdrMessageHelper)
+                .GetMethod("TranslateHeader", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            var header = BitConverter.GetBytes((ushort)((int)NetSdrClientApp.Messages.NetSdrMessageHelper.MsgTypes.DataItem0 << 13));
+            object[] parameters = new object[] { header, null, null };
+            method.Invoke(null, parameters);
+            // msgLength should be set to 8194
+            int msgLength = (int)parameters[2];
+            Assert.That(msgLength, Is.EqualTo(8194));
+        }
+
+        [Test]
+        public void GetSamples_EmptyBody_ReturnsEmpty()
+        {
+            var samples = NetSdrClientApp.Messages.NetSdrMessageHelper.GetSamples(16, new byte[0]);
+            Assert.That(samples.Count(), Is.EqualTo(0));
+        }
+
     }
 }
